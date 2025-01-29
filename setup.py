5449334ed1eb72661e99343b62d0200e9ee19366
Pedro H. <pedro_gonsalvs@hotmail.com>
2025-01-29 14:27:14 -0300

    fix: include package

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="project-generator",
     version="1.0",
     py_modules=["main"],
+    include_package_data=True,
     entry_points={
         "console_scripts": [
             "npt=main:main"
