47d14fed2b5e77562e06db8b15705b59e000e997
Pedro H. <pedro_gonsalves@hotmail.com>
2025-04-18 02:15:39 -0300

    refac: async connection

--- files/database/connection.py
+++ files/database/connection.py
@@ -1,11 +1,11 @@
-"""This module provides the connection to a database
-PostgreSQL. Check the .env file and make sure that
-the credentials are correct in the correct places.
+"""This module provides an async connection to a PostgreSQL database.
+Check the .env file and ensure credentials are correctly set.
 """
-import psycopg
-from psycopg import connection
 
-from logs.config import logger
+import psycopg
+from psycopg import AsyncConnection, AsyncCursor
+
+from log.config import logger
 from utils.env_var import get_env_var
 
 
@@ -16,23 +16,32 @@ class PgConnection:
         self._user_ = get_env_var('PG_USER')
         self._password_ = get_env_var('PG_PASSWORD')
         self._db_name_ = get_env_var('PG_NAME')
+        self._connection_: AsyncConnection | None = None
+        self.cursor: AsyncCursor | None = None
+
+    async def connect(self):
         try:
-            self._connection_: connection = psycopg.connect(
+            self._connection_ = await psycopg.AsyncConnection.connect(
                 host=self._host_,
                 port=self._port_,
                 user=self._user_,
                 password=self._password_,
                 dbname=self._db_name_
             )
-            self.cursor = self._connection_.cursor()
+            self.cursor = await self._connection_.cursor()
+            logger.info(f"New connection. ID: {self._connection_.fileno()}")
         except Exception as error:
-            logger.error(f"Error while connecting to database. {error}")
+            logger.error(f"Error while connecting to database: {error}")
 
-    def __enter__(self):
-        logger.info(f"New connection. ID: {self._connection_.fileno()}")
-        return self._connection_
+    async def close(self):
+        if self._connection_:
+            logger.info(f"Closed async connection. ID: {self._connection_.fileno()}")
+            await self._connection_.commit()
+            await self._connection_.close()
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        logger.info(f"Closed connection. ID: {self._connection_.fileno()}")
-        self._connection_.commit()
-        return self._connection_.close()
+    async def __aenter__(self):
+        await self.connect()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.close()
