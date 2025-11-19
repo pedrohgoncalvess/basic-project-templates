768ea93745c732333c10e845289f3c7315da830c
Pedro H. <pedro_gonsalves@hotmail.com>
2025-11-19 12:27:25 -0300

    add: the most veridic example of a migration

--- template_generator/files/database/migrations/20241210_01_LQSl5.sql
+++ template_generator/files/database/migrations/20241210_01_LQSl5.sql
@@ -1,7 +1,15 @@
 -- 
 -- depends: 
 
-create table teste (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(150)
-)
+CREATE SCHEMA "example";
+
+CREATE TABLE "example"."cep" (
+    id SERIAL,
+    cep VARCHAR(8) NOT NULL UNIQUE,
+    logradouro VARCHAR(250),
+    bairro VARCHAR(150),
+    cidade VARCHAR(100) NOT NULL,
+    uf VARCHAR(2) NOT NULL,
+
+    CONSTRAINT cep_pk PRIMARY KEY (id)
+);
